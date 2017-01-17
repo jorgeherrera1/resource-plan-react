40a5be79a0b7deb36283431157d46967213b2d93
Jorge Herrera <jorge.herrera.cabrera@gmail.com>
2017-01-16 22:08:00 -0600

    added resource info panel

---
+++ app/components/resource-plans-table/ResourceInfoPanel.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import ResourcePlanActions from '../../actions/ResourcePlanActions';
+import $ from 'jquery';
+import 'materialize';
+
+class ResourceInfoPanel extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.showPanel = this.showPanel.bind(this);
+  }
+
+  handleNameChanged(resourcePlanId, resourceName) {
+    ResourcePlanActions.updateResourceName(resourcePlanId, resourceName);
+  }
+
+  componentDidMount() {
+    $(this.node).modal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: .65, // Opacity of modal background
+      in_duration: 300, // Transition in duration
+      out_duration: 200 // Transition out duration
+    });
+  }
+
+  showPanel() {
+    $(this.node).modal('open');
+  }
+
+  render() {
+    const {resourcePlanId, resourceName} = this.props;
+
+    return (
+      <tr>
+        <td>
+          <div ref={(node) => (this.node = node)} className="modal modal-fixed-footer">
+            <div className="modal-content">
+              <h4>Resource Information</h4>
+              <p>{this.props.resourceName}</p>
+            </div>
+            <div className="modal-footer">
+              <button className="modal-action modal-close waves-effect waves-green btn-flat">
+                Remove
+              </button>
+            </div>
+          </div>
+          <div className="valign-wrapper">
+            <button
+              className="btn-floating waves-effect waves-light indigo darken-2"
+              onClick={this.showPanel}>
+              <i className="material-icons">info_outline</i>
+            </button>
+            <div style={{marginLeft: 'auto'}}>
+              {resourcePlanId + 1}
+            </div>
+          </div>
+        </td>
+        <td>
+          <input type="text"
+            value={resourceName}
+            onChange={(e) => {
+              this.handleNameChanged(resourcePlanId, e.target.value);
+            }} />
+        </td>
+      </tr>
+    );
+  }
+
+}
+
+ResourceInfoPanel.propTypes = {
+  resourcePlanId: React.PropTypes.number,
+  resourceName: React.PropTypes.string
+};
+
+export default ResourceInfoPanel;
--- app/components/resource-plans-table/ResourcesInformation.js
+++ app/components/resource-plans-table/ResourcesInformation.js
@@ -1,48 +1,14 @@
 import React from 'react';
-import ResourcePlanActions from '../../actions/ResourcePlanActions';
-import $ from 'jquery';
-import 'materialize';
+import ResourceInfoPanel from './ResourceInfoPanel';
 
 class ResourcesInformation extends React.Component {
-
-  handleNameChanged(resourcePlanId, resourceName) {
-    ResourcePlanActions.updateResourceName(resourcePlanId, resourceName);
-  }
 
   renderResourcesInformation() {
     const resourcesInformation = this.props.resourcePlans.map((resourcePlan, resourcePlanId) => {
       return (
-        <tr key={resourcePlanId}>
-          <td>
-            <div className="valign-wrapper">
-              <button className="btn-floating waves-effect waves-light indigo darken-2"
-                onClick={(e) => {
-                  console.log('clicked: ' + resourcePlanId);
-                  $('#modal1').modal({
-                    dismissible: true, // Modal can be dismissed by clicking outside of the modal
-                    opacity: .65, // Opacity of modal background
-                    in_duration: 300, // Transition in duration
-                    out_duration: 200, // Transition out duration
-                    starting_top: '40%', // Starting top style attribute
-                    ending_top: '70%'
-                  });
-                  $('#modal1').modal('open');
-                }}>
-                <i className="material-icons">info_outline</i>
-              </button>
-              <div style={{marginLeft: 'auto'}}>
-                {resourcePlanId + 1}
-              </div>
-            </div>
-          </td>
-          <td>
-            <input type="text"
-              value={resourcePlan.name}
-              onChange={(e) => {
-                this.handleNameChanged(resourcePlanId, e.target.value);
-              }} />
-          </td>
-        </tr>
+        <ResourceInfoPanel key={resourcePlanId}
+          resourcePlanId={resourcePlanId}
+          resourceName={resourcePlan.name} />
       );
     });
 
@@ -65,15 +31,6 @@ class ResourcesInformation extends React.Component {
             {resourcesInformation}
           </tbody>
         </table>
-        <div id="modal1" className="modal bottom-sheet">
-          <div className="modal-content">
-            <h4>Resource Information</h4>
-            <p>A chart is supposed to go here</p>
-          </div>
-          <div className="modal-footer">
-            <a href="#!" className="modal-action modal-close waves-effect waves-green btn-flat ">Remove</a>
-          </div>
-        </div>
       </div>
     );
   }
